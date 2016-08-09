ad089d1229b19db962d5688c9220d4e4ae9f07ba
root <root@vm-bc756f86.netangels.ru>
2016-08-09 18:42:00 +0500

    update run script

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ docker build -t web7 .
 docker run -tid \
 --name="web7" \
 -p 80:80 -p 443:443 \
--v `pwd`/..:/home/www \
+-v `pwd`/..:/home/www:rw \
 -v `pwd`/conf/etc/nginx/conf.d:/etc/nginx/conf.d:ro \
 -v `pwd`/conf/etc/mysql/my.cnf:/etc/mysql/my.cnf:ro \
 -v `pwd`/conf/usr/local/etc/php/conf.d/custom.ini:/usr/local/etc/php/conf.d/z-custom.ini:ro \
 -v `pwd`/conf/usr/local/etc/php-fpm.d:/usr/local/etc/php-fpm.d:ro \
--v `pwd`/db:/var/lib/mysql \
+-v `pwd`/db:/var/lib/mysql:rw \
 web7
